7dc04009f03730f54765db1961d3992ebff841bd
Lachinaafdev <alejandrafierrorm@gmail.com>
2022-09-23 02:02:41 -0500

    modified:main

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//evento del boton
+
 let home = document.getElementById ("home");
 let events = document.getElementById ("events");
 let news = document.getElementById ("news");
@@ -14,3 +16,44 @@ news.onclick = function(){
 events.onclick = function(){
     imgenchange.src ="./assets/producto.jpg"
 }
+
+//buscador
+
+
+ document.getElementById("botonsearch").addEventListener("click",mostrat_buscador);
+document.getElementById("botonsearch").addEventListener("click ", ocultar_buscador);
+
+ botonsearch = document.getElementById("botonsearch");
+ inputsearch = document.getElementById("inputsearch")
+
+ function mostrat_buscador(){
+  botonsearch.style.display = "block";
+  inputsearch.style.display = "block";
+  inputsearch.focus();
+    
+ }
+//buscador
+document.getElementById("inputsearch").addEventListener("keyup",buscador_interno);
+ function ocultar_buscador(){
+    botonsearch.style.display = "none";
+ }
+
+
+ function buscador_interno(){
+    filter = inputsearch.value.toUpperCase();
+    li = box-search.getElementByTagName();
+
+    for (i= 0; 1 <li.length;1++){
+        a=li[i].getElementByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+    if(textValue.toUpperCase().indexOf(filter)> -1){
+li[1].style.display=""
+ }
+ 
+
+
+ 
+
+ }
+ 
+ }
